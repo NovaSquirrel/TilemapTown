515bfbb3d8e4fc3a7713e116d26b3912c7ff39e4
NovaSquirrel <bushytail.squirrel@gmail.com>
2018-12-11 22:45:24 -0500

    /exportmap automatically downloads now

--- client/js/ui.js
+++ client/js/ui.js
@@ -226,7 +226,16 @@ function keyHandler(e) {
       if(chatInput.value.toLowerCase() == "/clear") {
         chatArea.innerHTML = "";
       } else if(chatInput.value.toLowerCase() == "/exportmap") {
-        logMessage('<a href="data:,'+encodeURI(exportMap())+'" download="map.txt">Map download (click here)</a>', 'server_message');
+        //logMessage('<a href="data:,'+encodeURIComponent(exportMap())+'" download="map.txt">Map download (click here)</a>', 'server_message');
+
+        //from https://ourcodeworld.com/articles/read/189/how-to-create-a-file-and-generate-a-download-with-javascript-in-the-browser-without-a-server
+        var element = document.createElement('a');
+        element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(exportMap()));
+        element.setAttribute('download', "map.txt");
+        element.style.display = 'none';
+        document.body.appendChild(element);
+        element.click();
+        document.body.removeChild(element);
       }
 
 
