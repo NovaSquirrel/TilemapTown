5542f351f186762d829328b82bf6631126857d54
NovaSquirrel <bushytail@novasquirrel.com>
2024-10-20 23:18:36 -0400

    Add some Greek, Coptic, Cyrillic to Tilemap Sans
    Extra characters contributed by hellomynameisjoe

--- tools/process_name_font.py
+++ tools/process_name_font.py
@@ -1,8 +1,5 @@
 from PIL import Image
 
-TILES_ACROSS = 16
-TILES_DOWN = 6
-
 def process_font(filename):
 	print(filename)
 	VOID_PIXEL = (255, 0, 255)
@@ -15,13 +12,17 @@ def process_font(filename):
 
 	pixels = list(im.getdata())
 	image_width, image_height = im.size
-	glyph_width = image_width // TILES_ACROSS
-	glyph_height = image_height // TILES_DOWN
+
+	glyph_width = 14
+	glyph_height = 16
+	tiles_across = image_width // glyph_width
+	tiles_down = image_height // glyph_height
 
 	width_per_character = []
 
-	for tile_y in range(TILES_DOWN):
-		for tile_x in range(TILES_ACROSS):
+	for tile_y in range(tiles_down):
+		for tile_x in range(tiles_across):
+			have_void = False
 			source_x = tile_x * glyph_width
 			source_y = tile_y * glyph_height
 			glyph_pixels = im.crop((source_x, source_y, source_x + glyph_width, source_y + glyph_height)).getdata()
@@ -31,10 +32,15 @@ def process_font(filename):
 			for pixel_y in range(glyph_height):
 				for pixel_x in range(glyph_width):
 					pixel = glyph_pixels[pixel_y*glyph_width+pixel_x]
+					if pixel == VOID_PIXEL:
+						have_void = True
 					if pixel == PAPER_PIXEL or pixel == INK_PIXEL or pixel == BORDER_AND_PAPER:
 						leftmost_x = min(pixel_x, leftmost_x)
 						rightmost_x = max(pixel_x, rightmost_x)
-			width_per_character.append(rightmost_x-leftmost_x+1)
+			if have_void:
+				width_per_character.append(rightmost_x-leftmost_x+1)
+			else:
+				width_per_character.append(-1)
 
 	print(width_per_character)
 
